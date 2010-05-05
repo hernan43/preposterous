898ee496ae32937aaecde4f9c8ab8ee14a9180a8
Ray Hernandez <hernan43@gmail.com>
2010-05-05 11:58:54 -0400

    Got started on the HTTP AUTH client code

--- lib/preposterous.rb
+++ lib/preposterous.rb
@@ -6,4 +6,5 @@ end
 # bring inall of the files in the lib dir
 directory = File.expand_path(File.dirname(__FILE__))
 require File.join(directory, "preposterous", "base")
+require File.join(directory, "preposterous", "httpauth")
 
