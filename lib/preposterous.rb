b3754ff6e691d55b950425cb4dee709e3b104cce
Ray Hernandez <hernan43@gmail.com>
2010-05-04 21:58:58 -0400

    More stubbing out of everything

--- lib/preposterous.rb
+++ lib/preposterous.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 module Preposterous
 end
 
--- lib/preposterous/base.rb
+++ lib/preposterous/base.rb
@@ -1,4 +1,21 @@
 module Preposterous
   class Base
+
+    def initialize(client)
+      @client = client
+    end
+
+    def getsites
+    end
+
+    def newpost
+    end
+
+    def updatepost
+    end    
+
+    def newcomment
+    end    
+
   end
 end
